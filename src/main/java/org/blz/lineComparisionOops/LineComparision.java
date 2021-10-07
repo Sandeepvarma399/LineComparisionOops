bfd5768e2df8ede7297de3fb54aaa45423f2dac2
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-07 11:27:59 +0530

    Added:Welcome Message

---
+++ src/main/java/org/blz/lineComparisionOops/LineComparision.java
@@ -0,0 +1,11 @@
+package org.blz.lineComparisionOops;
+
+
+public class LineComparision
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Welcome to line Comparision Computation Program" );
+    }
+}
